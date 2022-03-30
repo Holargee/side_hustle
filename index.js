fc96298420c8969077f28423f45c4fe2876f4171
Holargee <101959350+Holargee@users.noreply.github.com>
2022-03-30 18:54:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function validate(){
 		var e = document.getElementById("xnoti").style;
 			var f= document.getElementById("cnoti").style;
 	if(a.value=="user"&& b.value=="pass")
-{d.display="block";setTimeout(function(){location.href="side gig.html"},1500);}
+{d.display="block";setTimeout(function(){location.href="side hustle.html"},1500);}
 else if(a.value!=="user"){e.display="block";setTimeout(function(){e.display="none"},1000);}
 else{f.display="block";setTimeout(function(){f.display="none"},1000);}
 }
